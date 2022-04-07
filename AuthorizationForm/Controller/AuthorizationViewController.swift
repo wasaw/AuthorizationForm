49bee2b3d9d24a1581a6db7df39571b20b76da29
wasaw <merenkovalexander@gmail.com>
2022-04-07 10:47:54 +0300

    Создана форма страницы авторизации.

---
+++ AuthorizationForm/Controller/AuthorizationViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AuthorizationViewController.swift
+//  AuthorizationForm
+//
+//  Created by Александр Меренков on 4/6/22.
+//
+
+import UIKit
+
+class AuthorizationViewController: UIViewController {
+    
+//  MARK: - Outlet
+    
+    @IBOutlet weak var authorizationNavigationBar: UINavigationBar!
+    @IBOutlet weak var authorizationLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
